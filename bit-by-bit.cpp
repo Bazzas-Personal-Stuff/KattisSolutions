ab67bca2bfcf2a18b9c002a9910cc55626d455aa
Bazzagibbs <bailey.gibbons2018@gmail.com>
2021-01-11 01:12:56 +1100

    why isn't this solution working :(

--- bit-by-bit.cpp
+++ bit-by-bit.cpp
@@ -14,10 +14,10 @@ void _or(int idx1, int idx2) {
 
     if (bits[idx1] == '1' || bits[idx2] == '1') {
         bits[idx1] = '1';
-    } else if (bits[idx1] == '0' && bits[idx2] == '0') {
-        bits[idx1] == '0';
+    } else if (bits[idx1] == '?' || bits[idx2] == '?') {
+        bits[idx1] == '?';
     } else {
-        bits[idx1] = '?';
+        bits[idx1] = '0';
     }
 
     // 0 ? --> ?
@@ -26,10 +26,10 @@ void _or(int idx1, int idx2) {
 }
 
 void _and(int idx1, int idx2) {
-    if (bits[idx1] == '0' || bits[idx2] == '0') {
-        bits[idx1] == '0';
-    } else if (bits[idx1] == '1' && bits[idx2] == '1') {
-        bits[idx1] = '1';
+    if (bits[idx1] == '1' && bits[idx2] == '1') {
+        bits[idx1] == '1';
+    } else if (bits[idx1] == '0' || bits[idx2] == '0') {
+        bits[idx1] = '0';
     } else {
         bits[idx1] = '?';
     }
@@ -51,34 +51,48 @@ Store in bit 𝑖 the logical AND of the contents of bits 𝑖 and 𝑗
 */
 
 int main() {
-    int instructionCount = 0;
-    while (cin >> instructionCount) {
-        if (instructionCount == 0)
-            return 0;
-
+    int instructionCount;
+    while (true) {
+        cin >> instructionCount;
+        if (instructionCount == 0 || cin.eof())
+            break;
         for (int i = 0; i < 32; i++) {
             bits[i] = '?';
         }
+
         for (int i = 0; i < instructionCount; i++) {
-            string thisInstruction = "";
-            cin >> thisInstruction;
-            int arg1, arg2;
-            cin >> arg1;
-            if (thisInstruction == "CLEAR") {
-                _clear(arg1);
-            } else if (thisInstruction == "SET") {
+            string word;
+            cin >> word;
+            if (word == "SET") {
+                int arg1;
+                cin >> arg1;
+                arg1 = 31 - arg1;
                 _set(arg1);
-            } else if (thisInstruction == "OR") {
-                cin >> arg2;
-                _or(arg1, arg2);
-            } else if (thisInstruction == "AND") {
-                cin >> arg2;
+            }
+            if (word == "CLEAR") {
+                int arg1;
+                cin >> arg1;
+                arg1 = 31 - arg1;
+                _clear(arg1);
+            }
+            if (word == "AND") {
+                int arg1, arg2;
+                cin >> arg1 >> arg2;
+                arg1 = 31 - arg1;
+                arg2 = 31 - arg2;
                 _and(arg1, arg2);
             }
+            if (word == "OR") {
+                int arg1, arg2;
+                cin >> arg1 >> arg2;
+                arg1 = 31 - arg1;
+                arg2 = 31 - arg2;
+                _or(arg1, arg2);
+            }
         }
-        for (int i = 31; i >= 0; i--) {
-            cout << bits[i];
+        for (char c : bits) {
+            cout << c;
         }
-        cout << endl;
+        cout << '\n';
     }
 }
