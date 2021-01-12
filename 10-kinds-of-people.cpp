8e4584ed8c665e80f1e2856ed534ce866b49bda5
Bazzagibbs <bailey.gibbons2018@gmail.com>
2021-01-12 17:32:51 +1100

    working on 10-kinds-of-people

---
+++ 10-kinds-of-people.cpp
@@ -0,0 +1,125 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+using namespace std;
+/*
+    World map is stored as a 2d array of chars
+    We only need one bit to store world information, so a "visited" flag
+    can be stored in the second bit, used for pathfinding.
+
+    0000 0011
+           │└─ value (binary/decimal)
+           └── "visited" flag
+
+    Value and flag can be accessed with bitwise AND
+        char value = world[x][y] & 1;
+        bool flag = world[x][y] & 2 > 1;
+*/
+vector<vector<char>> world;
+bool checkValidPoint(pair<int, int> point, char type) {
+    if (point.first >= 0 && point.first < world[0].size() &&
+        point.second >= 0 && point.second < world.size()) { // in map bounds
+        char element = world[point.first][point.second];
+        if (!(element & 2 > 1) &&
+            (element & 1) == type) { // not visited and same type
+            return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    map<pair<int, int>, int> sectorMap;
+
+    int xMax, yMax;
+    cin >> yMax >> xMax;
+    for (int i = 0; i < yMax; i++) {
+        for (int j = 0; j < xMax; j++) {
+            char c;
+            cin >> c;
+            world[j][i] = c - '0'; // convert from ascii to number
+        }
+    }
+
+    int queryCount;
+    int sectorCount = 0;
+    cin >> queryCount;
+    for (int i = 0; i < queryCount; i++) {
+        pair<int, int> origin, destination;
+        string originString;
+        cin >> origin.second >> origin.first >> destination.second >>
+            destination.first;
+
+        // normalize to start at 0
+        origin.first--;
+        origin.second--;
+        destination.first--;
+        destination.second--;
+
+        char originType = world[origin.first][origin.second] & 1;
+        if (originType == 0) {
+            originString = "binary";
+        } else {
+            originString = "decimal";
+        }
+
+        // don't need to pathfind if they're different types
+        if (world[origin.first][origin.second] !=
+            world[destination.first][destination.second]) {
+            cout << "neither\n";
+            continue;
+        }
+
+        // check if points are already in mapped sectors
+        int originInMap = sectorMap.count(origin);
+        int destinationInMap = sectorMap.count(destination);
+
+        // if both are not in map, scan from origin first before continuing.
+        if (originInMap == 0 && destinationInMap == 0) {
+            vector<pair<int, int>> thisIter;
+            vector<pair<int, int>> nextIter;
+            thisIter.push_back(origin);
+
+            while (!thisIter.empty()) {
+                for (int i = 0; i < thisIter.size(); i++) {
+
+                    pair<int, int> coord = thisIter[i];
+                    sectorMap.insert(
+                        pair<pair<int, int>, int>(coord, sectorCount));
+                    pair<int, int> n = coord, s = coord, e = coord, w = coord;
+                    n.second++;
+                    s.second--;
+                    e.first++;
+                    w.first--;
+                    pair<int, int> possible[] = {n, s, e, w};
+
+                    for (pair<int, int> p : possible) {
+                        if (checkValidPoint(p, originType)) {
+                            nextIter.push_back(p);
+                            world[p.first][p.second] =
+                                world[p.first][p.second] & 2;
+                        }
+                    }
+                }
+                thisIter.swap(nextIter);
+                nextIter.clear();
+            }
+
+            sectorCount++;
+        }
+
+        // if they both are, check sector id
+        if (originInMap > 0 && destinationInMap > 0) {
+            if (sectorMap[origin] == sectorMap[destination]) {
+                cout << originType << '\n';
+            } else {
+                cout << "neither\n";
+            }
+        }
+        // if only one is, they're in different sectors
+        else {
+            cout << "neither\n";
+        }
+    }
+}
