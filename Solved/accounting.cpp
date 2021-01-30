bb18407af8009742daea8fe99788e38828991bd7
Bazzagibbs <bailey.gibbons2018@gmail.com>
2021-01-30 19:51:52 +1100

    added accounting, teque

---
+++ Solved/accounting.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <map>
+using namespace std;
+
+int defaultAmount;
+map<int, int> people;
+
+void SET(int person, int amount) { people[person] = amount; }
+
+void RESTART(int amount) {
+    people.clear();
+    defaultAmount = amount;
+}
+
+void PRINT(int person) {
+    if (people.count(person) != 0) {
+        cout << people[person] << endl;
+    } else {
+        cout << defaultAmount << endl;
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    int N;
+    int Q;
+
+    cin >> N >> Q;
+
+    string instruction;
+    int a;
+    int b;
+
+    for (int i = 0; i < Q; i++) {
+        cin >> instruction;
+        cin >> a;
+        if (instruction.compare("SET") == 0) {
+            cin >> b;
+            SET(a, b);
+        } else if (instruction.compare("RESTART") == 0) {
+            RESTART(a);
+        } else if (instruction.compare("PRINT") == 0) {
+            PRINT(a);
+        }
+    }
+}
---
+++ Solved/teque.cpp
@@ -0,0 +1,75 @@
+/*
+1. push_back x: insert the element 𝑥 into the back of the teque.
+2. push_front x: insert the element 𝑥 into the front of the teque.
+3. push_middle x: insert the element 𝑥 into the middle of the teque.
+The inserted element 𝑥 now becomes the new middle element of the teque.
+If 𝑘 is the size of the teque before the insertion, the insertion index for 𝑥 is (𝑘+1)/2 (using 0-based indexing).
+*/
+
+#include <deque>
+#include <iostream>
+#include <string>
+
+using namespace std;
+deque<int> frontHalf, backHalf;
+
+void balance() {
+    int expected   = (frontHalf.size() + backHalf.size() + 1) / 2;
+    int difference = frontHalf.size() - expected;
+    int toMove;
+    if (difference > 0) {
+        toMove = frontHalf.back();
+        backHalf.push_front(toMove);
+        frontHalf.pop_back();
+    } else if (difference < 0) {
+        toMove = backHalf.front();
+        frontHalf.push_back(toMove);
+        backHalf.pop_front();
+    }
+}
+
+void push_back(int element) {
+    balance();
+    backHalf.push_back(element);
+}
+
+void push_front(int element) {
+    balance();
+    frontHalf.push_front(element);
+}
+
+void push_middle(int element) {
+    balance();
+    backHalf.push_front(element);
+}
+
+int get(int index) {
+    if (index < frontHalf.size()) {
+        return frontHalf[index];
+    } else {
+        return backHalf[index - frontHalf.size()];
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+
+    int n;
+    cin >> n;
+
+    string instruction;
+    int value;
+
+    for (int i = 0; i < n; i++) {
+        cin >> instruction >> value;
+        if (instruction.compare("get") == 0) {
+            cout << get(value) << '\n';
+        } else if (instruction.compare("push_back") == 0) {
+            push_back(value);
+        } else if (instruction.compare("push_front") == 0) {
+            push_front(value);
+        } else if (instruction.compare("push_middle") == 0) {
+            push_middle(value);
+        }
+    }
+}
