c41af2afd2b41f835e7ac0ac4ffa3f6a9f8e909a
Bazzagibbs <bailey.gibbons2018@gmail.com>
2021-01-31 14:45:01 +1100

    fixed comments

--- Solved/toast.cpp
+++ Solved/toast.cpp
@@ -10,11 +10,11 @@
 
     chord formula
         l = 2r sin(Θ/2)
-        r = 1/2sin(Θ/2)
+        r = l/2sin(Θ/2) // solve for r
 
-        l = 2d so
-        2d/2sin(Θ/2) = r
-        d/sin(Θ/2) = r
+            l = 2d so
+        r = 2d/2sin(Θ/2)
+        r = d/sin(Θ/2)
 */
 
 #include <cmath>
@@ -30,12 +30,11 @@ int main() {
     cin >> n >> d >> t;
 
     angleBetweenPeople = (2 * M_PI) / n; // radians
-    clinksPerPerson    = t * 2 / n;      // might only need half (used to be (t*2)/n)
+    clinksPerPerson    = t * 2 / n;
     if (clinksPerPerson + 1 == n) {
         minR = 0;
         maxR = d;
     } else {
-
         minR = d / sin((clinksPerPerson / 2 + 1) * angleBetweenPeople / 2);
         maxR = d / sin(clinksPerPerson / 2 * angleBetweenPeople / 2);
     }
